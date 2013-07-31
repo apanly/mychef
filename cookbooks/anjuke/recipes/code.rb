15ddae0a1530f1e8a2332358152af64f8c44142a
杨勇 <eyong4736@163.com>
2013-07-31 13:47:11 +0800

    pass anjuke page

--- cookbooks/anjuke/recipes/code.rb
+++ cookbooks/anjuke/recipes/code.rb
@@ -9,11 +9,11 @@ execute "clone php code repo" do
 end
 
 # checkout pages files
-execute "clone pages repo" do
-  user node['anjuke']['username']
-  cmds = [
-          "cd /code",
-          "svn checkout #{node['anjuke']['page_repo']} --username=#{node['anjuke']['svn_user']} --password=#{node['anjuke']['svn_pass']} pages"
-  ]
-  command (cmds.join " ; ")
-end
+#execute "clone pages repo" do
+#  user node['anjuke']['username']
+#  cmds = [
+#          "cd /code",
+#          "svn checkout #{node['anjuke']['page_repo']} --username=#{node['anjuke']['svn_user']} --password=#{node['anjuke']['svn_pass']} pages"
+#  ]
+#  command (cmds.join " ; ")
+#end
